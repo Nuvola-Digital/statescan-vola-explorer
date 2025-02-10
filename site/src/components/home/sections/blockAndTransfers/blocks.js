72f1b86baf27da6c364d30dfe886200585293cea
vattevaii_ashish <ashish@ngit.com.np>
2025-02-10 09:40:24 +0545

    fix: view all blocks button was disabled

--- site/src/components/home/sections/blockAndTransfers/blocks.js
+++ site/src/components/home/sections/blockAndTransfers/blocks.js
@@ -1,19 +1,17 @@
 import { Anchor, AnchorWrapper, Section, StyledPanel, Title } from "../styled";
 import React from "react";
 import LatestBlocks from "../latestBlocks";
-import { getIsSimpleMode } from "../../../../utils/env";
 import useLatestBlocks from "../../../../hooks/overview/useLatestBlocks";
 
 export default function Blocks() {
   const { blocks, loading } = useLatestBlocks();
-  const isSimpleMode = getIsSimpleMode();
 
   return (
     <Section>
       <Title>Latest Blocks</Title>
       <StyledPanel>
         <LatestBlocks blocks={blocks} loading={loading} />
-        <AnchorWrapper disabled={isSimpleMode || !blocks.length || loading}>
+        <AnchorWrapper disabled={!blocks.length || loading}>
           <Anchor to={"/blocks"}>View All</Anchor>
         </AnchorWrapper>
       </StyledPanel>
